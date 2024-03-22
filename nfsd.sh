c350146401a755d41b41e446f0a99c817903b34d
Ryan Wicks <99614749+ryancwicks-voyis@users.noreply.github.com>
2024-03-22 16:02:04 -0400

    Update nfsd.sh
    Turn on nfsv3 support

--- nfsd.sh
+++ nfsd.sh
@@ -103,12 +103,12 @@ while true; do
     /sbin/rpcinfo
 
     # Only required if v3 will be used
-    # /usr/sbin/rpc.idmapd
-    # /usr/sbin/rpc.gssd -v
-    # /usr/sbin/rpc.statd
+    /usr/sbin/rpc.idmapd
+    /usr/sbin/rpc.gssd -v
+    /usr/sbin/rpc.statd
 
     echo "Starting NFS in the background..."
-    /usr/sbin/rpc.nfsd --debug 8 --no-udp --no-nfs-version 2 --no-nfs-version 3
+    /usr/sbin/rpc.nfsd --debug 8 --no-udp --no-nfs-version 2 
     echo "Exporting File System..."
     if /usr/sbin/exportfs -rv; then
       /usr/sbin/exportfs
@@ -117,7 +117,7 @@ while true; do
       exit 1
     fi
     echo "Starting Mountd in the background..."These
-    /usr/sbin/rpc.mountd --debug all --no-udp --no-nfs-version 2 --no-nfs-version 3
+    /usr/sbin/rpc.mountd --debug all --no-udp --no-nfs-version 2
 # --exports-file /etc/exports
 
     # Check if NFS is now running by recording it's PID (if it's not running $pid will be null):
